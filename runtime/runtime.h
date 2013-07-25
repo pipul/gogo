f0a268f9ebe3f29eb6d0743b3fd93e3bfdce5853
Dong Fang <yp.fangdong@gmail.com>
2013-07-25 12:02:18 -0400

    fix runtime.h header file

--- runtime/runtime.h
+++ runtime/runtime.h
@@ -1,5 +1,7 @@
 /* Copyright (c) 2013 Dong Fang, MIT; see COPYRIGHT */
 
+#ifndef _RUNTIME_H_
+#define _RUNTIME_H_
 
 // spinlock
 #include <pthread.h>
@@ -12,3 +14,7 @@ extern void spinlock_init(void *lock);
 extern void spinlock_destroy(void *lock);
 extern void spin_lock(void *lock);
 extern void spin_unlock(void *lock);
+
+
+
+#endif
